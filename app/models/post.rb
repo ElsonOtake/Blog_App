2384d53ebc3ac72cb58b5208cf677c9c2599433b
Elson Otake <elsonotake@yahoo.com.br>
2022-09-14 20:31:51 -0300

    Add method to return recent comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+  def recent_comments
+    Comment.order(created_at: :desc).first(5)
+  end
 end
