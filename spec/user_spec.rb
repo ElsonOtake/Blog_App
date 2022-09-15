587998dea08c5f32f6432a7d7adf2c35dbe83e66
Elson Otake <elsonotake@yahoo.com.br>
2022-09-15 14:31:13 -0300

    Add tests for User class

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'Name must not be blank'
-  it 'PostsCounter must be an integer greater than or equal to zero'
+  user = User.new(name: 'Anything')
+  it 'is valid with valid attributes' do
+    # user = User.new(name: 'Anything')
+    expect(user).to be_valid
+  end
+  it 'is not valid without a name' do
+    user.name = nil
+    expect(user).to_not be_valid
+  end
+  it 'Name must not be blank' do
+    user.name = ' '
+    expect(user).to_not be_valid
+  end
+  it 'PostsCounter must be an integer greater than or equal to zero' do
+    expect(user.post_counter).to be_an(Numeric)
+    expect(user.post_counter).to be >= 0
+  end
 end
