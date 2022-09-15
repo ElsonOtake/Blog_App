90774b553de5c285428defbda2beff6ae9084421
Elson Otake <elsonotake@yahoo.com.br>
2022-09-15 14:43:33 -0300

    Remove unused comment

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   user = User.new(name: 'Anything')
   it 'is valid with valid attributes' do
-    # user = User.new(name: 'Anything')
     expect(user).to be_valid
   end
   it 'is not valid without a name' do
