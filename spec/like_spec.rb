e0f926eedbdad7766825392cde140c58bdc3ddae
Elson Otake <elsonotake@yahoo.com.br>
2022-09-15 17:24:21 -0300

    Add tests for update_likes_counter method

--- spec/like_spec.rb
+++ spec/like_spec.rb
@@ -21,4 +21,26 @@ RSpec.describe Like, type: :model do
   it 'Post_id must be the post.id' do
     expect(like.post_id).to be(post.id)
   end
+  it 'Likes counter must have value 1' do
+    new_user = User.create(name: 'Anyone else')
+    new_post = Post.create(title: 'Something else', author: new_user)
+    new_like = Like.create(author: new_user, post: new_post)
+    new_like.update_likes_counter
+    expect(new_post.likes_counter).to be(1)
+  end
+  it 'Likes counter must have value 5' do
+    new_user_a = User.create(name: 'Anyone else 1')
+    new_user_b = User.create(name: 'Anyone else 2')
+    new_user_c = User.create(name: 'Anyone else 3')
+    new_user_d = User.create(name: 'Anyone else 4')
+    new_user_e = User.create(name: 'Anyone else 5')
+    new_post = Post.create(title: 'Something else', author: new_user_a)
+    Like.create(author: new_user_a, post: new_post)
+    Like.create(author: new_user_b, post: new_post)
+    Like.create(author: new_user_c, post: new_post)
+    Like.create(author: new_user_d, post: new_post)
+    new_like = Like.create(author: new_user_e, post: new_post)
+    new_like.update_likes_counter
+    expect(new_post.likes_counter).to be(5)
+  end
 end
