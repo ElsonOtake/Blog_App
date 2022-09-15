ac98667e4c6faf783cff2cb57c6e9f33df44df7c
Elson Otake <elsonotake@yahoo.com.br>
2022-09-15 15:46:32 -0300

    Add test for method recent_posts

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  user = User.new(name: 'Anything')
+  user = User.new(name: 'Anyone')
   it 'is valid with valid attributes' do
     expect(user).to be_valid
   end
@@ -16,5 +16,13 @@ RSpec.describe User, type: :model do
   it 'PostsCounter must be an integer greater than or equal to zero' do
     expect(user.post_counter).to be_an(Numeric)
     expect(user.post_counter).to be >= 0
+  end
+  it 'Recent posts must be empty array' do
+    expect(user.recent_posts.size).to be(0)
+  end
+  it 'Recent posts must be empty array' do
+    Post.new(title: 'Anything', author: user)
+    Post.new(title: 'Other thing', author: user)
+    expect(user.recent_posts.size).to be(0)
   end
 end
