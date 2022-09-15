984d26a831dd4a0d0eb6093d2016aaca9ddf4665
Elson Otake <elsonotake@yahoo.com.br>
2022-09-15 16:11:36 -0300

    Add validation to text field

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+  validates :text, presence: true, length: { maximum: 250, too_long: "%{count} characters is the maximum allowed" }
 
   def update_comments_counter
     post.update(comments_counter: post.comments.count)
