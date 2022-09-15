eef5bfc70bd142265a2fe267f2775a6a59958f6f
Elson Otake <elsonotake@yahoo.com.br>
2022-09-15 14:48:43 -0300

    Add validation to title maximum size

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 250, too_long: "%{count} characters is the maximum allowed" }
 
   after_save :update_user_counter
 
