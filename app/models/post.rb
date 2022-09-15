f1789aa4f2f29953a402d42a614092476a269db3
Elson Otake <elsonotake@yahoo.com.br>
2022-09-15 14:44:15 -0300

    Add tests for Post class

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+  validates :title, presence: true
 
   after_save :update_user_counter
 
--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -1,8 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it 'Title must not be blank'
+  user = User.new(name: 'Anyone')
+  post = Post.new(title: 'Anything', author: user)
+  it 'is valid with valid attributes' do
+    expect(post).to be_valid
+  end
+  it 'Title must not be blank' do
+    post.title = ' '
+    expect(post).to_not be_valid
+  end
   it 'Title must not exceed 250 characters'
-  it 'CommentsCounter must be an integer greater than or equal to zero'
-  it 'LikesCounter must be an integer greater than or equal to zero'
+  it 'CommentsCounter must be an integer greater than or equal to zero' do
+    expect(post.comments_counter).to be_an(Numeric)
+    expect(post.comments_counter).to be >= 0
+  end
+  it 'LikesCounter must be an integer greater than or equal to zero' do
+    expect(post.likes_counter).to be_an(Numeric)
+    expect(post.likes_counter).to be >= 0
+  end
 end
