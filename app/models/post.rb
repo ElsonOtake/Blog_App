a61735fc96aeb06391357e31110aecc3fb7b4899
Elson Otake <elsonotake@yahoo.com.br>
2022-09-14 22:19:54 -0300

    Add method to update user post counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
   def recent_comments
-    Comment.order(created_at: :desc).first(5)
+    comments.order(created_at: :desc).first(5)
+  end
+
+  def update_user_counter
+    author.update(post_counter: author.posts.count)
   end
 end
