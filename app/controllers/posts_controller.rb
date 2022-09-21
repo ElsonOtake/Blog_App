64163a9da9fb3d3439010ca0da116f515b2c10b0
Elson Otake <elsonotake@yahoo.com.br>
2022-09-21 14:23:14 -0300

    Remove boundaries check

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,8 @@ class PostsController < ApplicationController
   def index
     @posts_per_page = 2
     @user = User.find(params[:user_id])
-    @page = params.fetch(:page, 1).to_i
-    @page = 1 if @page < 1
-    aux = ((@user.posts.size + 1) / @posts_per_page)
-    @page = aux if @page > aux
-    @posts = @user.posts[2 * (@page - 1), @posts_per_page]
+    @page = params.fetch(:page, 1)
+    @posts = @user.posts[2 * (@page.to_i - 1), @posts_per_page]
   end
 
   def show
