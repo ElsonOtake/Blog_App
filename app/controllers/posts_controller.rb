51780dbd4c484eb6027cfdff46ed7a23667762d8
Elson Otake <elsonotake@yahoo.com.br>
2022-09-21 13:22:00 -0300

    Update page number fetch

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,8 @@ class PostsController < ApplicationController
   POSTS_PER_PAGE = 2
   def index
     @user = User.find(params[:user_id])
-    @page = 0
-    @posts = @user.posts[2 * @page, POSTS_PER_PAGE]
+    @page = params.fetch(:page, 0)
+    @posts = @user.posts[2 * @page.to_i, POSTS_PER_PAGE]
   end
 
   def show
