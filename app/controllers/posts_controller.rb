28922eacc0cd92dfa609f5a45d23aceb6f00193f
Elson Otake <elsonotake@yahoo.com.br>
2022-09-21 13:08:42 -0300

    Update posts per page constant variable

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,9 @@
 class PostsController < ApplicationController
+  POSTS_PER_PAGE = 2
   def index
     @user = User.find(params[:user_id])
     @page = 0
-    @posts = @user.posts[2 * @page, 2]
+    @posts = @user.posts[2 * @page, POSTS_PER_PAGE]
   end
 
   def show
