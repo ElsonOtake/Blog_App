ffd1b4e383d2b0b7d28654a5823a71a93ccdc6d7
Elson Otake <elsonotake@yahoo.com.br>
2022-09-21 13:55:26 -0300

    Fix pages boundaries

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,11 @@ class PostsController < ApplicationController
   POSTS_PER_PAGE = 2
   def index
     @user = User.find(params[:user_id])
-    @page = params.fetch(:page, 0)
-    @posts = @user.posts[2 * @page.to_i, POSTS_PER_PAGE]
+    @page = params.fetch(:page, 1).to_i
+    @page = 1 if @page < 1
+    aux = ((@user.posts.size + 1) / POSTS_PER_PAGE)
+    @page = aux if @page > aux
+    @posts = @user.posts[2 * (@page - 1), POSTS_PER_PAGE]
   end
 
   def show
