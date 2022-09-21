4f19c639d28d856328e1306ee41a67d4757214dc
Elson Otake <elsonotake@yahoo.com.br>
2022-09-21 17:03:08 -0300

    Add after save call

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,8 @@ class Comment < ApplicationRecord
   belongs_to :post
   validates :text, presence: true, length: { maximum: 250, too_long: '%<count>s characters is the maximum allowed' }
 
+  after_save :update_comments_counter
+
   def update_comments_counter
     post.update(comments_counter: post.comments.count)
   end
