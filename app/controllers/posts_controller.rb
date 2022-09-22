dea2bf51791d5ba493661a8ae92318032da6c75d
Elson Otake <elsonotake@yahoo.com.br>
2022-09-22 13:39:18 -0300

    Add current user for all view methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    @current_user = current_user
     @posts_per_page = 2
     @user = User.find(params[:user_id])
     @page = params.fetch(:page, 1)
@@ -7,6 +8,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @current_user = current_user
     @post = Post.find(params[:id])
   end
 
@@ -21,19 +23,19 @@ class PostsController < ApplicationController
   def create
     post = Post.new(post_params)
 
-    # respond_to do |format|
-    respond_to do
-
-      if post.save
-        flash[:success] = 'Post was successfully created'
-        redirect_to root_url
-        # format.html { redirect_to post_url(post), notice: "Post was successfully created." }
-        # format.json { render :show, status: :created, location: post }
-      else
-        flash.now[:error] = 'Error: Post could not be saved'
-        render :new, post
-        # format.html { render :new, status: :unprocessable_entity }
-        # format.json { render json: post.errors, status: :unprocessable_entity }
+    respond_to do |format|
+      format.html do
+        if post.save
+          flash[:success] = 'Post was successfully created'
+          redirect_to root_url
+          # format.html { redirect_to post_url(post), notice: "Post was successfully created." }
+          # format.json { render :show, status: :created, location: post }
+        else
+          flash.now[:error] = 'Error: Post could not be saved'
+          render :new, post
+          # format.html { render :new, status: :unprocessable_entity }
+          # format.json { render json: post.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
