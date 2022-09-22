afb2d654ba3cb869c18ce68c0077d4b5e7863660
Elson Otake <elsonotake@yahoo.com.br>
2022-09-22 13:29:37 -0300

    Add create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,32 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+    @current_user = current_user
+    respond_to do |format|
+      format.html { render :new, locals: { post: @post } }
+    end
+  end
+
+  def create
+    post = Post.new(post_params)
+
+    # respond_to do |format|
+    respond_to do
+
+      if post.save
+        flash[:success] = 'Post was successfully created'
+        redirect_to root_url
+        # format.html { redirect_to post_url(post), notice: "Post was successfully created." }
+        # format.json { render :show, status: :created, location: post }
+      else
+        flash.now[:error] = 'Error: Post could not be saved'
+        render :new, post
+        # format.html { render :new, status: :unprocessable_entity }
+        # format.json { render json: post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
