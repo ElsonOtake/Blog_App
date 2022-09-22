454352819e027b2a9a23a908768f9e93011f226b
Elson Otake <elsonotake@yahoo.com.br>
2022-09-22 16:43:36 -0300

    Generate likes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'likes/create'
+  get 'comments/create'
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
       resources :comments, only: %i[create]
