ae1b5d85051e89eccacbc43b36b71c04dc313e5e
Elson Otake <elsonotake@yahoo.com.br>
2022-09-22 12:01:17 -0300

    Update routes for posts new and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index show new create]
   end
   get 'users/index'
   get 'users/show'
