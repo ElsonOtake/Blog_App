ee31523469e96846cc4ee5866a54113b82073531
Elson Otake <elsonotake@yahoo.com.br>
2022-09-22 16:28:52 -0300

    Add routes for comments and likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create]
+    resources :posts, only: %i[index show new create] do
+      resources :comments, only: %i[create]
+      resources :likes, only: %i[create]
+    end
   end
   get 'users/index'
   get 'users/show'
