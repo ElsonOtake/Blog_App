ea79bb781739f8b519602c67e3fa397f1a3bc1bf
Elson Otake <elsonotake@yahoo.com.br>
2022-09-27 14:39:34 -0300

    Add tests for user#index

--- spec/features/users_index_spec.rb
+++ spec/features/users_index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe 'user index view', type: :feature do
-  describe 'GET index for Lilly' do
+RSpec.describe 'User index view', type: :feature do
+  describe 'GET Lilly index' do
     before(:each) do
       @lilly = User.create(name: 'Lilly', photo: 'https://c.tenor.com/YIeHLcvImMsAAAAM/meditation-dog.gif',
                            bio: 'Teacher from Poland')
@@ -14,62 +14,57 @@ RSpec.describe 'user index view', type: :feature do
       expect(page).to have_xpath('/')
     end
 
-    it 'Show username of each user' do
+    it 'Show the username of all other users' do
       visit root_path
       expect(page).to have_content('Lilly')
       expect(page).to_not have_content('Luna')
     end
 
-    it 'Shows the uses profile picture' do
+    it 'Shows the profile picture for each user' do
       visit root_path
       expect(page).to have_css('img[src*="meditation-dog"]')
       expect(page).to_not have_css('img[src*="puppy-stretching"]')
     end
 
-    it 'Shows the number of posts of each user' do
+    it 'Shows the number of posts each user has written' do
       visit root_path
       expect(page).to have_content('Number of posts: 2')
       expect(page).to_not have_content('Number of posts: 1')
     end
 
-    it "click on name 'Lilly' to redirect to user's show page" do
+    it "When I click on a user, I am redirected to that user's show page" do
       visit root_path
       click_link 'Lilly'
       expect(current_path).to eq user_path(@lilly)
     end
   end
 
-  describe 'GET index' do
+  describe 'GET Luna index' do
     before(:each) do
       @luna = User.create(name: 'Luna', photo: 'https://c.tenor.com/JAWsyDUCa4QAAAAM/puppy-stretching.gif',
                           bio: 'Teacher from Brazil')
       Post.create(author: @luna, title: 'Hi', text: 'This is my first post')
     end
 
-    it 'Show the user index view path' do
-      visit root_path
-      expect(page).to have_xpath('/')
-    end
-
-    it 'Show username of each user' do
+    it 'Show the username of all other users' do
       visit root_path
       expect(page).to have_content('Luna')
       expect(page).to_not have_content('Lilly')
     end
 
-    it 'Shows the uses profile picture' do
+    it 'Shows the profile picture for each user' do
       visit root_path
       expect(page).to have_css('img[src*="puppy-stretching"]')
       expect(page).to_not have_css('img[src*="meditation-dog"]')
     end
 
-    it 'Shows the number of posts of each user' do
+    it 'Shows the number of posts each user has written' do
       visit root_path
       expect(page).to have_content('Number of posts: 1')
       expect(page).to_not have_content('Number of posts: 2')
     end
 
-    it "click on name 'Luna' to redirect to user's show page" do
+    it "When I click on a user, I am redirected to that user's show page" do
       visit root_path
       click_link 'Luna'
       expect(current_path).to eq user_path(@luna)
