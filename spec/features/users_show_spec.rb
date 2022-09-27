b16b588e4914568d310f9f3c307634778dcbbbb4
Elson Otake <elsonotake@yahoo.com.br>
2022-09-26 22:08:45 -0300

    Update to original content

--- spec/features/users_show_spec.rb
+++ spec/features/users_show_spec.rb
@@ -1,23 +1,22 @@
 require 'rails_helper'
-require_relative '../config/environment'
 
 RSpec.describe 'The show user page', type: :feature do
   before(:each) do
-    @user = User.create(name: 'Addisu', photo: 'img_1.png', bio: 'Physics teacher', posts_counter: 2)
-    @user.save!
-    @post_one = Post.create(title: 'First post', text: 'Hello world!', id: 1, comments_counter: 0, likes_counter: 0)
-    @post_two = Post.create(title: 'Second post', text: 'Quantum Mechanics!', id: 2, comments_counter: 0,
-                            likes_counter: 0)
+    @user = User.create(name: 'Lilly', photo: 'https://c.tenor.com/YIeHLcvImMsAAAAM/meditation-dog.gif',
+                        bio: 'Teacher from Poland')
+    # @user.save!
+    @post_one = Post.create(title: 'First post', text: 'Hello world!', author: @user)
+    @post_two = Post.create(title: 'Second post', text: 'Quantum Mechanics!', author: @user)
 
-    visit(user_path(id: @user))
+    # visit(user_path(id: @user))
   end
 
-  it 'show profile pictures fo the user' do
+  it 'show profile pictures for the user' do
     expect(page).to have_css('img[src*="img_1.png"]')
   end
 
   it 'shows username of the user' do
-    expect(page).to have_content('Addisu')
+    expect(page).to have_content('Lilly')
   end
 
   it 'shows the number of posts for the user' do
