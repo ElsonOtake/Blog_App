65d16bb05225b579c7a8bbdae7f9a64c3f632dd9
Elson Otake <elsonotake@yahoo.com.br>
2022-09-28 09:27:32 -0300

    Fix linter error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :bio, :photo) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :bio, :photo) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :password, :current_password, :bio, :photo)
+    end
   end
 end
