6d3588c8ad26fd2f591aed4903a8bd306f7af2bd
Elson Otake <elsonotake@yahoo.com.br>
2022-09-28 18:05:51 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'likes/create'
-  get 'comments/create'
+
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create] do
-      resources :comments, only: %i[create]
+    resources :posts, only: %i[index show new create delete] do
+      resources :comments, only: %i[create delete]
       resources :likes, only: %i[create]
     end
   end
-  get 'users/index'
-  get 'users/show'
-  get 'posts/index'
-  get 'posts/show'
+
   root 'users#index'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
