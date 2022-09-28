32d3279a5515092aebf13b28116299ce16aa0619
Elson Otake <elsonotake@yahoo.com.br>
2022-09-28 19:24:48 -0300

    Add delete route

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   end
 
   root 'users#index'
-  
+
+  delete '/users/:user_id/posts/:id', to: 'posts#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
