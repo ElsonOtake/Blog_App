4b1fc2bce0ca406b53145c4308877dc58f861064
Elson Otake <elsonotake@yahoo.com.br>
2022-09-28 19:52:08 -0300

    Add comment destroy method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -22,4 +22,16 @@ class CommentsController < ApplicationController
       end
     end
   end
+
+  def destroy
+    user = User.find(params[:user_id])
+    post = Post.find(params[:post_id])
+    comment = Comment.find(params[:id])
+    comment.destroy
+
+    respond_to do |format|
+      format.html { redirect_to user_post_path(user, post), notice: 'Comment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 end
