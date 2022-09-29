46164575ca01cf93bec001c32cdcb1089690939c
jevazquezb <je.vazquezb@gmail.com>
2022-09-29 14:29:06 -0500

    Edit model's ability file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,23 @@ class Ability
   def initialize(user)
     user ||= User.new
 
-    can :destroy, Post, author_id: user.id
-    can :destroy, Comment, author_id: user.id
-    can :destroy, :all if user.is? :admin
+    if user.is? :admin
+      can :manage, :all
+    else
+      can :destroy, Post do |post|
+        post.user == user
+      end
+
+      can :destroy, Comment do |comment|
+        comment.user == user
+      end
+
+      can :create, Post
+      can :create, Comment
+    end
+
+    # can :destroy, Post, author_id: user.id
+    # can :destroy, Comment, author_id: user.id
+    # can :manage, :all if user.is? :admin
   end
 end
