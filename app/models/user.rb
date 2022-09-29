ff47efeecca2f3ac78d978a49095a5f14f9e4fd4
Elson Otake <elsonotake@yahoo.com.br>
2022-09-29 13:43:27 -0300

    Add method to check role

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,10 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  def is?(requested_role)
+    role == requested_role.to_s
+  end
+
   def recent_posts
     posts.order(created_at: :desc).first(3)
   end
