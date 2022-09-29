84fb6711fa9b3ffa08873d8a790100f6e9a468ac
Elson Otake <elsonotake@yahoo.com.br>
2022-09-29 17:06:41 -0300

    Update authorizations for users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,23 +4,8 @@ class Ability
   def initialize(user)
     user ||= User.new
 
-    if user.is? :admin
-      can :manage, :all
-    else
-      can :destroy, Post do |post|
-        post.user == user
-      end
-
-      can :destroy, Comment do |comment|
-        comment.user == user
-      end
-
-      can :create, Post
-      can :create, Comment
-    end
-
-    # can :destroy, Post, author_id: user.id
-    # can :destroy, Comment, author_id: user.id
-    # can :manage, :all if user.is? :admin
+    can :destroy, Post, author_id: user.id
+    can :destroy, Comment, author_id: user.id
+    can :manage, :all if user.is? :admin
   end
 end
