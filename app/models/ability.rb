f446281d1b058f9340c575f589aaf7280dab8b5a
Elson Otake <elsonotake@yahoo.com.br>
2022-09-29 16:03:58 -0300

    Create authorizations for users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,11 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Handle the case where we don't have a current_user i.e. the user is a guest
     user ||= User.new
 
-    alias_action :create, :read, :update, :destroy, to: :crud
-
-    can :crud, :all if user.is? :admin
+    can :destroy, Post, author_id: user.id
+    can :destroy, Comment, author_id: user.id
+    can :destroy, :all if user.is? :admin
   end
 end
