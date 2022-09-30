cafc74d61dda95861e120fcf69383ef6465c784a
Elson Otake <elsonotake@yahoo.com.br>
2022-09-30 17:12:06 -0300

    Update check users and posts

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,31 +1,38 @@
 class Api::V1::CommentsController < ApplicationController
+  before_action :authorize_request
+  before_action :find_user_post
+
   ALLOWED_DATA = %(text).freeze
+
   def index
-    post = Post.find(params[:post_id])
-    comments = post.comments
+    comments = @post.comments
     render json: comments
   end
 
   def show
-    post = Post.find(params[:post_id])
-    comment = post.comments.find(params[:id])
+    comment = @post.comments.find(params[:id])
     render json: comment
   end
 
   def create
     data = json_payload.select { |allow| ALLOWED_DATA.include?(allow) }
-    p data
     post = Post.find(params[:post_id])
-    p post
     comment = Comment.new(data)
-    p comment
     comment.post = post
-    comment.author = User.first
-    p comment
+    comment.author = @current_user
     if comment.save
       render json: comment
     else
       render json: { error: 'could not create it' }
     end
+  end
+
+  private
+
+  def find_user_post
+    user = User.find_by_id!(params[:user_id])
+    @post = user.posts.find_by_id!(params[:post_id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { errors: 'User and/or post not found' }, status: :not_found
   end
 end
