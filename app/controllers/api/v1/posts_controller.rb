8b1f8c670243319056425af00d7a0226e52a3db4
Elson Otake <elsonotake@yahoo.com.br>
2022-09-30 17:11:29 -0300

    Update check user

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,11 +1,29 @@
 class Api::V1::PostsController < ApplicationController
+  before_action :find_user_post
+
   def index
-    posts = Post.all
-    render json: posts
+    posts = @user.posts
+    if posts.size.positive?
+      render json: posts
+    else
+      render json: { errors: 'Posts not found' }, status: :not_found
+    end
   end
 
   def show
-    post = Post.find(params[:id])
-    render json: post
+    post = @user.posts.find(params[:id])
+    if post
+      render json: post
+    else
+      render json: { errors: 'Post not found' }, status: :not_found
+    end
+  end
+
+  private
+
+  def find_user_post
+    @user = User.find_by_id!(params[:user_id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { errors: 'User not found' }, status: :not_found
   end
 end
