3c303d7b13b352b26e54d21f91b42b772e5d0a82
Elson Otake <elsonotake@yahoo.com.br>
2022-09-30 20:11:43 -0300

    Update test to unregistered email

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -6,7 +6,7 @@ class AuthenticationController < ApplicationController
   # POST /auth/login
   def login
     data = json_payload.select { |allow| ALLOWED_DATA.include?(allow) }
-    @user = User.find_by_email(data[:email])
+    @user = User.find_by_email!(data[:email])
     if @user.valid_password?(data[:password])
       token = JsonWebToken.encode(user_id: @user.id)
       time = Time.now + 24.hours.to_i
@@ -15,5 +15,7 @@ class AuthenticationController < ApplicationController
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
     end
+  rescue ActiveRecord::RecordNotFound
+    render json: { errors: 'User and/or email not found' }, status: :not_found
   end
 end
