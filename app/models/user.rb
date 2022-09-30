542fc70d9d486867c1f048c94be82350c4f7bddc
Elson Otake <elsonotake@yahoo.com.br>
2022-09-30 15:16:52 -0300

    Add has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :comments, foreign_key: 'author_id'
