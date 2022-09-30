d4db0ebb7d95318d278909475b75a0e59ba543c6
Elson Otake <elsonotake@yahoo.com.br>
2022-09-30 12:00:59 -0300

    Add api post controller

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::PostsController < ApplicationController
+  def index
+    posts = Post.all
+    render json: posts
+  end
+
+  def show
+    post = Post.find(params[:id])
+    render json: post
+  end
+end
