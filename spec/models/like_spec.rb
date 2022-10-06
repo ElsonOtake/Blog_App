b6056d0810aa9d6d4719bc4807226c4bec68fc1d
Elson Otake <elsonotake@yahoo.com.br>
2022-10-05 23:30:58 -0300

    Fix linter error

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -38,11 +38,16 @@ RSpec.describe Like, type: :model do
   end
 
   it 'Likes counter must have value 5' do
-    @new_user_a = User.create(name: 'Anyone else 1', email: 'user_a@blog.com', password: 'password', confirmed_at: Time.now)
-    @new_user_b = User.create(name: 'Anyone else 2', email: 'user_b@blog.com', password: 'password', confirmed_at: Time.now)
-    @new_user_c = User.create(name: 'Anyone else 3', email: 'user_c@blog.com', password: 'password', confirmed_at: Time.now)
-    @new_user_d = User.create(name: 'Anyone else 4', email: 'user_d@blog.com', password: 'password', confirmed_at: Time.now)
-    @new_user_e = User.create(name: 'Anyone else 5', email: 'user_e@blog.com', password: 'password', confirmed_at: Time.now)
+    @new_user_a = User.create(name: 'Anyone else 1', email: 'user_a@blog.com', password: 'password',
+                              confirmed_at: Time.now)
+    @new_user_b = User.create(name: 'Anyone else 2', email: 'user_b@blog.com', password: 'password',
+                              confirmed_at: Time.now)
+    @new_user_c = User.create(name: 'Anyone else 3', email: 'user_c@blog.com', password: 'password',
+                              confirmed_at: Time.now)
+    @new_user_d = User.create(name: 'Anyone else 4', email: 'user_d@blog.com', password: 'password',
+                              confirmed_at: Time.now)
+    @new_user_e = User.create(name: 'Anyone else 5', email: 'user_e@blog.com', password: 'password',
+                              confirmed_at: Time.now)
     @new_post = Post.create(title: 'Something else', author: @new_user_a)
     Like.create(author: @new_user_a, post: @new_post)
     Like.create(author: @new_user_b, post: @new_post)
