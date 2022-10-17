af6c7bbfba1a35c54e5c7edcefe848424a8bd21a
Elson Otake <elsonotake@yahoo.com.br>
2022-10-16 23:40:01 -0300

    Update post api documentation

--- spec/integration/posts_spec.rb
+++ spec/integration/posts_spec.rb
@@ -3,35 +3,28 @@ require 'swagger_helper'
 describe 'Posts' do
   path '/api/v1/users/{id}/posts' do
     get 'Retrieves posts' do
+      security [{ ApiKeyAuth: [] }]
       tags 'Posts'
       produces 'application/json'
-      parameter name: :id, in: :path, type: :string
+      parameter name: :id, in: :path, type: :string, required: true, description: 'user id'
 
       response '200', 'OK' do
-        schema type: :object,
-               properties: {
-                 id: { type: :integer },
-                 title: { type: :string },
-                 text: { type: :string },
-                 comments_counter: { type: :integer },
-                 likes_counter: { type: :integer },
-                 created_at: { type: :string },
-                 updated_at: { type: :string },
-                 author_id: { type: :integer }
-               }
-        let(:id) do
-          { id: 1 }
-        end
+        schema type: :object, properties: {
+          id: { type: :integer },
+          title: { type: :string },
+          text: { type: :string },
+          comments_counter: { type: :integer },
+          likes_counter: { type: :integer },
+          created_at: { type: :string },
+          updated_at: { type: :string },
+          author_id: { type: :integer }
+        }
+        let(:comments_counter) { 0 }
         run_test!
       end
 
       response '404', 'Not found' do
-        let(:id) { { error: 'Posts not found' } }
-        run_test!
-      end
-
-      response '404', 'Not found' do
-        let(:id) { { error: 'User not found' } }
+        let(:error) { 'User/Post not found' }
         run_test!
       end
     end
@@ -39,35 +32,29 @@ describe 'Posts' do
 
   path '/api/v1/users/{user_id}/posts/{id}' do
     get 'Retrieve a post' do
+      security [{ ApiKeyAuth: [] }]
       tags 'Posts'
       produces 'application/json'
-      parameter name: :id, in: :path, type: :string
+      parameter name: :user_id, in: :path, type: :string, required: true, description: 'user id'
+      parameter name: :id, in: :path, type: :string, required: true, description: 'post id'
 
       response '200', 'OK' do
-        schema type: :object,
-               properties: {
-                 id: { type: :integer },
-                 title: { type: :string },
-                 text: { type: :string },
-                 comments_counter: { type: :integer },
-                 likes_counter: { type: :integer },
-                 created_at: { type: :string },
-                 updated_at: { type: :string },
-                 author_id: { type: :integer }
-               }
-        let(:id) do
-          { user_id: 1, id: 1 }
-        end
+        schema type: :object, properties: {
+          id: { type: :integer },
+          title: { type: :string },
+          text: { type: :string },
+          comments_counter: { type: :integer },
+          likes_counter: { type: :integer },
+          created_at: { type: :string },
+          updated_at: { type: :string },
+          author_id: { type: :integer }
+        }
+        let(:comments_counter) { 0 }
         run_test!
       end
 
       response '404', 'Not found' do
-        let(:id) { { error: 'Post not found' } }
-        run_test!
-      end
-
-      response '404', 'Not found' do
-        let(:id) { { error: 'User not found' } }
+        let(:error) { 'User/Post not found' }
         run_test!
       end
     end
