d693ba945fb544bbc662d0dfe2477afb1d8a08c9
Elson Otake <elsonotake@yahoo.com.br>
2022-10-16 23:40:20 -0300

    Update comment api documentation

--- spec/integration/comments_spec.rb
+++ spec/integration/comments_spec.rb
@@ -3,20 +3,21 @@ require 'swagger_helper'
 describe 'Comments' do
   path '/api/v1/users/{user_id}/posts/{post_id}/comments' do
     get 'Retrieves comments' do
+      security [{ ApiKeyAuth: [] }]
       tags 'Comments'
       produces 'application/json'
-      parameter name: :id, in: :path, type: :string
+      parameter name: :user_id, in: :path, type: :string, required: true, description: 'user id'
+      parameter name: :post_id, in: :path, type: :string, required: true, description: 'post id'
 
       response '200', 'OK' do
-        schema type: :object,
-               properties: {
-                 id: { type: :integer },
-                 text: { type: :string },
-                 created_at: { type: :string },
-                 updated_at: { type: :string },
-                 author_id: { type: :integer },
-                 post_id: { type: :integer }
-               }
+        schema type: :object, properties: {
+          id: { type: :integer },
+          text: { type: :string },
+          created_at: { type: :string },
+          updated_at: { type: :string },
+          author_id: { type: :integer },
+          post_id: { type: :integer }
+        }
         let(:id) do
           { authorization: token }
         end
@@ -24,7 +25,7 @@ describe 'Comments' do
       end
 
       response '404', 'Not found' do
-        let(:id) { { error: 'User and/or post not found' } }
+        let(:error) { 'User and/or post not found' }
         run_test!
       end
     end
@@ -32,33 +33,28 @@ describe 'Comments' do
 
   path '/api/v1/users/{user_id}/posts/{post_id}/comments/{id}' do
     get 'Retrieve a comment' do
+      security [{ ApiKeyAuth: [] }]
       tags 'Comments'
       produces 'application/json'
-      parameter name: :id, in: :path, type: :string
+      parameter name: :user_id, in: :path, type: :string, required: true, description: 'user id'
+      parameter name: :post_id, in: :path, type: :string, required: true, description: 'post id'
+      parameter name: :id, in: :path, type: :string, required: true, description: 'comment id'
 
       response '200', 'OK' do
-        schema type: :object,
-               properties: {
-                 id: { type: :integer },
-                 text: { type: :string },
-                 created_at: { type: :string },
-                 updated_at: { type: :string },
-                 author_id: { type: :integer },
-                 post_id: { type: :integer }
-               }
-        let(:header) do
-          { authorization: token }
-        end
+        schema type: :object, properties: {
+          id: { type: :integer },
+          text: { type: :string },
+          created_at: { type: :string },
+          updated_at: { type: :string },
+          author_id: { type: :integer },
+          post_id: { type: :integer }
+        }
+        let(:post_id) { :post_id }
         run_test!
       end
 
       response '404', 'Not found' do
-        let(:id) { { error: 'Comment not found' } }
-        run_test!
-      end
-
-      response '404', 'Not found' do
-        let(:id) { { error: 'User and/or post not found' } }
+        let(:error) { 'User/Post/Comment not found' }
         run_test!
       end
     end
@@ -66,30 +62,33 @@ describe 'Comments' do
 
   path '/api/v1/users/{user_id}/posts/{post_id}/comments' do
     post 'Create a comment' do
+      security [{ ApiKeyAuth: [] }]
       tags 'Comments'
       consumes 'application/json'
+      parameter name: :user_id, in: :path, type: :string, required: true, description: 'user id'
+      parameter name: :post_id, in: :path, type: :string, required: true, description: 'post id'
       parameter name: :id, in: :body, description: 'Create a comment', schema: {
-        type: :object,
-        properties: {
-          text: { type: :string }
-        },
+        type: :object, properties: {
+                         text: { type: :string }
+                       },
         required: %w[text]
       }
 
       response '200', 'OK' do
-        let(:id) do
-          { id: 1,
-            text: 'This is a new comment',
-            created_at: '2022-10-02T16:50:59.888Z',
-            updated_at: '2022-10-02T16:50:59.888Z',
-            author_id: 18,
-            post_id: 27 }
-        end
+        schema type: :object, properties: {
+          id: { type: :integer },
+          text: { type: :string },
+          created_at: { type: :string },
+          updated_at: { type: :string },
+          author_id: { type: :integer },
+          post_id: { type: :integer }
+        }
+        let(:post_id) { :post_id }
         run_test!
       end
 
       response '404', 'Not found' do
-        let(:id) { { error: 'User and/or post not found' } }
+        let(:error) { 'User and/or post not found' }
         run_test!
       end
     end
