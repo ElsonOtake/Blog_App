95c12817d092684b55d347b02d507982185c00e7
Elson Otake <elsonotake@yahoo.com.br>
2023-06-13 23:16:28 -0300

    Add has avatar

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,6 +5,8 @@ class Member < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
+  has_one_attached :avatar
+
   validates :name, presence: true
   validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
