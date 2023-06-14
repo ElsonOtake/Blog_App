8b1ed359111a6725278bc1768e2a51ad8631ecd0
Elson Otake <elsonotake@yahoo.com.br>
2023-06-13 23:33:36 -0300

    Create option do display thumb size image

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,7 +5,9 @@ class Member < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
-  has_one_attached :avatar
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [110, 110]
+  end
 
   validates :name, presence: true
   validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
