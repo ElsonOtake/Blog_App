9de79b1cf98990494af1890a6956584613bf364a
Elson Otake <elsonotake@yahoo.com.br>
2023-06-16 23:49:22 -0300

    Update before action

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,5 @@
 class MembersController < ApplicationController
-  before_action :authenticate_member!, except: [:index]
+  before_action :authenticate_member!
   load_and_authorize_resource
 
   def index
