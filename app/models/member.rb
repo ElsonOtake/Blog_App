bd97760161b9109967ce46acca212de5f93d9d6f
Elson Otake <elsonotake@yahoo.com.br>
2023-06-17 20:53:33 -0300

    Update action to display avatar

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,9 +5,7 @@ class Member < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
-  has_one_attached :avatar do |attachable|
-    attachable.variant :thumb, resize_to_limit: [110, 110]
-  end
+  has_one_attached :avatar
 
   validates :name, presence: true
   validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
@@ -15,6 +13,10 @@ class Member < ApplicationRecord
     less_than: 1.megabytes,
     message: 'is greater than 1 megabyte'
   }
+
+  def avatar_as_thumbnail
+    avatar.variant(resize_to_limit: [48, 48]).processed
+  end
 
   def is?(requested_role)
     role == requested_role.to_s
