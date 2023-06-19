c2b00ae625de9498284253f10678684dbb168f41
Elson Otake <elsonotake@yahoo.com.br>
2023-06-19 09:24:09 -0300

    Update js method to add and remove likes

--- app/javascript/controllers/stimulus_controller.js
+++ app/javascript/controllers/stimulus_controller.js
@@ -30,17 +30,18 @@ export default class extends Controller {
     fetch(`../../../api/v1/members/${user_id}/posts/${post_id}/likes/0`, configObj);
   }
 
-  toggle_like() {
-    const icon = document.getElementById("likes_counter");
-    const counter = document.querySelector(".counter");
-    if (icon.className == "bi-star") {
-      icon.classList.add("bi-star-fill");
-      icon.classList.remove("bi-star");
+  toggle_likes() {
+    console.log("Toggle likes started");
+    const icon = document.getElementById(`like_icon_${this.postValue}`);
+    const counter = document.getElementById(`likes_counter_${this.postValue}`);
+    if (icon.classList[0] == "fa-regular") {
+      icon.classList.add("fa-solid");
+      icon.classList.remove("fa-regular");
       this.add_like();
       counter.innerHTML = parseInt(counter.innerHTML) + 1;
     } else {
-      icon.classList.add("bi-star");
-      icon.classList.remove("bi-star-fill");
+      icon.classList.add("fa-regular");
+      icon.classList.remove("fa-solid");
       this.remove_like();
       counter.innerHTML = parseInt(counter.innerHTML) - 1;
     }
