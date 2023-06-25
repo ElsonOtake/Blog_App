7116962aa71361988ec8a7b8af3a0e3093a26bce
Elson Otake <elsonotake@yahoo.com.br>
2023-06-24 21:12:18 -0300

    Add bulma modal

--- app/javascript/controllers/stimulus_controller.js
+++ app/javascript/controllers/stimulus_controller.js
@@ -67,4 +67,45 @@ export default class extends Controller {
       counter.innerHTML = parseInt(counter.innerHTML) + 1;
     }
   }
+
+  openModal($el) {
+    $el.classList.add('is-active');
+  }
+
+  closeModal($el) {
+    $el.classList.remove('is-active');
+  }
+
+  closeAllModals() {
+    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+      this.closeModal($modal);
+    });
+  }
+
+  open_modal() {
+    const $target = document.getElementById("modal-blog");
+    
+    this.openModal($target);
+  }
+
+  // Add a click event on various child elements to close the parent modal
+  initialize() {
+    (document.querySelectorAll('.modal-background, .modal-close') || []).forEach(($close) => {
+      const $target = $close.closest('.modal');
+      
+      $close.addEventListener('click', () => {
+        this.closeModal($target);
+      });
+    });
+
+    // Add a keyboard event to close all modals
+    document.addEventListener('keydown', (event) => {
+      const e = event;
+      console.log(e.code);
+      if (e.code === "Escape") { // Escape key
+        this.closeAllModals();
+      }
+    });
+  }
+
 }
