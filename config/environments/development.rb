0a13431b384599755f40771dd572528497c0deea
Elson Otake <elsonotake@yahoo.com.br>
2023-07-09 20:52:13 -0300

    Create pagination tag from second page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,12 +2,12 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.after_initialize do
-    Bullet.enable        = true
-    Bullet.alert         = true
-    Bullet.bullet_logger = true
-    Bullet.console       = true
-    Bullet.rails_logger  = true
-    Bullet.add_footer    = true
+    Bullet.enable        = false
+    Bullet.alert         = false
+    Bullet.bullet_logger = false
+    Bullet.console       = false
+    Bullet.rails_logger  = false
+    Bullet.add_footer    = false
   end
 
   # Settings specified here will take precedence over those in config/application.rb.
