b973f5372e521ea154a79f445fee2b2850851cad
Elson Otake <elsonotake@yahoo.com.br>
2023-07-09 21:55:53 -0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,40 +3,59 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
 
 unless Member.any?
-  Member.create!(name: "Elson", bio: "Student from Brazil", email: "elson@blog.com", password: "password", password_confirmation: "password")
-  Member.create!(name: "Worash", bio: "Student from Ethiopia", email: "worash@blog.com", password: "password", password_confirmation: "password")
-  Member.create!(name: "Jesus", bio: "Student from Mexico", email: "jesus@blog.com", password: "password", password_confirmation: "password")
-  Member.create!(name: "Darik", bio: "Student from Ethiopia", email: "darik@blog.com", password: "password", password_confirmation: "password")
-  Member.create!(name: "Gabriel", bio: "Student from Brazil", email: "gabriel@blog.com", password: "password", password_confirmation: "password")
-  Member.create!(name: "David", bio: "Student from Ecuador", email: "david@blog.com", password: "password", password_confirmation: "password")
+  Member.create!(name: 'Elson', bio: 'Student from Brazil', email: 'elson@blog.com', password: 'foobar', password_confirmation: 'foobar')
+  Member.create!(name: 'Worash', bio: 'Student from Ethiopia', email: 'worash@blog.com', password: 'foobar', password_confirmation: 'foobar')
+  Member.create!(name: 'Jesus', bio: 'Student from Mexico', email: 'jesus@blog.com', password: 'foobar', password_confirmation: 'foobar')
+  Member.create!(name: 'Darik', bio: 'Student from Ethiopia', email: 'darik@blog.com', password: 'foobar', password_confirmation: 'foobar')
+  Member.create!(name: 'Gabriel', bio: 'Student from Brazil', email: 'gabriel@blog.com', password: 'foobar', password_confirmation: 'foobar')
+  Member.create!(name: 'David', bio: 'Student from Ecuador', email: 'david@blog.com', password: 'foobar', password_confirmation: 'foobar')
+  Member.create!(name: 'Brenda', bio: 'Student from Rwanda', email: 'brenda@blog.com', password: 'foobar', password_confirmation: 'foobar')
+  Member.create!(name: 'Addisu', bio: 'Student from Ethiopia', email: 'addisu@blog.com', password: 'foobar', password_confirmation: 'foobar')
+  Member.create!(name: 'Tinashe', bio: 'Student from Zimbabwe', email: 'tinashe@blog.com', password: 'foobar', password_confirmation: 'foobar')
+  Member.create!(name: 'Gabriel', bio: 'Student from Brazil', email: 'santo@blog.com', password: 'foobar', password_confirmation: 'foobar')  
+  Member.create!(name: 'Arturo', bio: 'Student from Ecuador', email: 'arturo@blog.com', password: 'foobar', password_confirmation: 'foobar')
 end
 
 unless Post.any?
-  members_id = Member.pluck(:id)
-  members_id.each do |id|
-    member = Member.find(id)
-    Faker::Number.between(from: 1, to: 25).times do
+  members = Member.all
+  members.each do |member|
+    Faker::Number.between(from: 1, to: 50).times do
       member.posts.create!(
-        title: Faker::Lorem.sentence,
-        text: Faker::Lorem.unique.paragraph(sentence_count: 2)
+        title: Faker::Quote.famous_last_words,
+        text: Faker::Quote.jack_handey
       )
     end
   end
 end
 
 unless Comment.any?
-  post_id = Post.pluck(:id)
-  post_id.each do |id|
-    post = Post.find(id)
-    Faker::Number.between(from: 0, to: 10).times do
+  member_ids = Member.pluck(:id)
+  posts = Post.all
+  posts.each do |post|
+    Faker::Number.between(from: 0, to: 20).times do
       post.comments.create!(
-        text: Faker::Lorem.paragraph(sentence_count: 2),
-        author_id: members_id[Faker::Number.between(from: 1, to: members_id.size) - 1]
+        text: Faker::Quote.most_interesting_man_in_the_world,
+        author_id: member_ids[Faker::Number.between(from: 1, to: member_ids.size) - 1]
       )
+    end
+  end
+end
+
+unless Like.any?
+  member_ids = Member.pluck(:id)
+  posts = Post.all
+  posts.each do |post|
+    aux = []
+    Faker::Number.between(from: 0, to: member_ids.size).times do
+      member = member_ids[Faker::Number.between(from: 1, to: member_ids.size) - 1]
+      aux << member unless aux.include? member
+    end
+    aux.each do |id|
+      post.likes.create!(author_id: id)
     end
   end
 end
