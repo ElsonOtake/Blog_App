f61403d2ae4c4dbb3f9df51b195699b98df468d0
Elson Otake <elsonotake@yahoo.com.br>
2023-11-29 17:49:22 -0300

    Remove background job to deploy on render

--- app/controllers/concerns/track_event.rb
+++ app/controllers/concerns/track_event.rb
@@ -2,7 +2,8 @@ module TrackEvent
   extend ActiveSupport::Concern
 
   def track_event
-    # puts "************************** size #{session[:comment_size]}"
-    CreateCounterJob.perform_async(session[:action], session[:post_author])
+    event = CounterAnalytic.where(action: session[:action], member_id: session[:post_author]).first_or_create!
+    event.count += 1
+    event.save!
   end
 end
