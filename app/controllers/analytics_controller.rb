79602ce9deb6d92d730caa84bed612eef8695b80
Elson Otake <elsonotake@yahoo.com.br>
2023-11-29 14:14:37 -0300

    Create chart using js

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,7 +1,9 @@
 class AnalyticsController < ApplicationController
   def index
-    @data = []
+    @start_date = params[:start_date] || (Date.today - 6.days).to_s
+    @end_date = params[:end_date] || Date.today.to_s
     @counter_analytics = current_user.counter_analytics.order(created_at: :desc)
+    @data = []
     @counter_analytics.each do |count|
       @data << { name: count.action, data: { count.created_at.to_s => count.count } }
     end
--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -2,9 +2,51 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="chart"
 export default class extends Controller {
-  static targets = [ "counterAnalytic" ]
+  static targets = [ "counterAnalytic", "counter" ]
+  static values = { startDate: String, endDate: String }
+
+  connect = () => {
+    this.counterChart = new Chartkick.ColumnChart(this.counterTarget, this.counterData);
+  }
 
   counterAnalyticTargetConnected = () => {
-    console.log("Counter analytics connected");
+    if (this.counterChart) {
+      this.counterChart.updateData(this.counterData);
+    };
+  }
+
+  createDateRange = () => {
+    const result = {};
+    const currentDate = new Date(this.startDateValue);
+  
+    while (currentDate <= new Date(this.endDateValue)) {
+      const formattedDate = currentDate.toISOString().split('T')[0];
+      result[formattedDate] = 0;
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+  
+    return result;
+  }
+
+  loadCounterData = (data, target, value) => {
+    if (value > 0) {
+      if (!data[target.dataset.action]) {
+        data[target.dataset.action] = {
+          name: target.dataset.action,
+          data: this.createDateRange()
+        };
+      }
+      if (data[target.dataset.action] != null && data[target.dataset.action].data[target.dataset.created] != null) {
+        data[target.dataset.action].data[target.dataset.created] += value;
+      };
+    }
+  }
+
+  counterData = (success) => {
+    let data = {};
+    this.counterAnalyticTargets.forEach(target => {
+      this.loadCounterData(data, target, parseInt(target.dataset.count, 10));
+    });
+    success(Object.values(data));
   }
 }
