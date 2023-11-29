d3acdcc5bc28688c8c02f29a17c1e577fe55cac6
Elson Otake <elsonotake@yahoo.com.br>
2023-11-29 18:02:33 -0300

    Comment the sidekiq job call

--- app/controllers/concerns/track_event.rb
+++ app/controllers/concerns/track_event.rb
@@ -2,6 +2,7 @@ module TrackEvent
   extend ActiveSupport::Concern
 
   def track_event
+    # CreateCounterJob.perform_async(session[:action], session[:post_author])
     event = CounterAnalytic.where(action: session[:action], member_id: session[:post_author]).first_or_create!
     event.count += 1
     event.save!
