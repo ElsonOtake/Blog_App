223fd12a83a01cfeb71999f493de52f635b0e1f9
Elson Otake <elsonotake@yahoo.com.br>
2023-11-29 10:35:39 -0300

    Create analytics controller/view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'analytics/index', as: 'analytics'
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   devise_for :members, controllers: { omniauth_callbacks: 'members/omniauth_callbacks',
