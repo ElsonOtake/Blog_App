f0569067a46ca295defba1c47174993b4caf97ff
Elson Otake <elsonotake@yahoo.com.br>
2023-11-29 13:02:06 -0300

    Create counter chart

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,4 +1,9 @@
 class AnalyticsController < ApplicationController
   def index
+    @data = []
+    @counter = current_user.counter_analytics.order(created_at: :desc)
+    @counter.each do |count|
+      @data << { name: count.action, data: { count.created_at.to_s => count.count } }
+    end
   end
 end
