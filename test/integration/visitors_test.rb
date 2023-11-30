0738f5c572666aa789c9473b0281b81a655efed2
Elson Otake <elsonotake@yahoo.com.br>
2023-11-30 19:14:45 -0300

    Create visitors integration tests

---
+++ test/integration/visitors_test.rb
@@ -0,0 +1,33 @@
+require "test_helper"
+require "sidekiq/testing"
+
+class VisitorsIntegrationTest < ActionDispatch::IntegrationTest
+  setup do
+    # A test fake that pushes all jobs into a jobs array
+    Sidekiq::Testing.fake!
+  end
+
+  def after_teardown
+    # clear all workers' jobs
+    Sidekiq::Worker.clear_all
+  end
+  
+  test "should not create Visitor when visiting the root path" do
+    assert_difference("Visitor.count", 0) do
+      get root_url
+    end
+  end
+  
+  test "should reuse the visitor during the session" do
+    get root_url
+    assert_difference("Visitor.count", 0) do
+      get analytics_url
+    end
+  end
+  
+  test "should record the signed in member" do
+    sign_in members(:regular)
+    get root_url
+    assert_equal Visitor.last.reload.member, members(:regular)
+  end
+end
