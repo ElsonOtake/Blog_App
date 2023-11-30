4a3ce320fd4b412b1c62d2b63566b0f0d6ca70f8
Elson Otake <elsonotake@yahoo.com.br>
2023-11-30 17:43:14 -0300

    Create association with visitors

--- app/models/browser_analytic.rb
+++ app/models/browser_analytic.rb
@@ -1,5 +1,6 @@
 class BrowserAnalytic < ApplicationRecord
   belongs_to :member
+  belongs_to :visitor
 
   broadcasts_to lambda { |browser_analytic|
                   [browser_analytic.member_id, 'browser_analytics']
