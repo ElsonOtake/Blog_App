b7d8b26604982ff0f88a8044aaed8ccea5aedf07
Elson Otake <elsonotake@yahoo.com.br>
2023-11-30 17:44:44 -0300

    Add tests to analytics controller

---
+++ test/controllers/analytics_controller_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class AnalyticsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index if user signed in" do
+    sign_in members(:regular)
+    get analytics_url
+    assert_response :success
+  end
+
+  test "should not get index without user signed in" do
+    get analytics_url
+    assert_response :redirect
+  end
+end
