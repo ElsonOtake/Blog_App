28dedce959f9db26805a808fe9fc405877c7b618
Elson Otake <elsonotake@yahoo.com.br>
2023-11-29 22:05:47 -0300

    Add time interval range

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -2,9 +2,21 @@ class AnalyticsController < ApplicationController
   def index
     @start_date = params[:start_date] || (Date.today - 6.days).to_s
     @end_date = params[:end_date] || Date.today.to_s
-    @counter_analytics = current_user.counter_analytics.order(created_at: :desc)
-    @length_analytics = current_user.length_analytics.order(created_at: :desc)
-    @unique_analytics = current_user.unique_analytics.order(created_at: :desc)
-    @browser_analytics = current_user.browser_analytics.order(created_at: :desc)
+    @counter_analytics = current_user
+      .counter_analytics
+      .where("created_at BETWEEN ? AND ?", @start_date + ' 00:00:00', @end_date + ' 23:59:59')
+      .order(created_at: :desc)
+    @length_analytics = current_user
+      .length_analytics
+      .where("created_at BETWEEN ? AND ?", @start_date + ' 00:00:00', @end_date + ' 23:59:59')
+      .order(created_at: :desc)
+    @unique_analytics = current_user
+      .unique_analytics
+      .where("created_at BETWEEN ? AND ?", @start_date + ' 00:00:00', @end_date + ' 23:59:59')
+      .order(created_at: :desc)
+    @browser_analytics = current_user
+      .browser_analytics
+      .where("created_at BETWEEN ? AND ?", @start_date + ' 00:00:00', @end_date + ' 23:59:59')
+      .order(created_at: :desc)
   end
 end
