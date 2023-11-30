afea7139e4b8e3e9a03785082a7bc422ab0b100b
Elson Otake <elsonotake@yahoo.com.br>
2023-11-30 12:04:51 -0300

    Update to a dryer code

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -2,21 +2,11 @@ class AnalyticsController < ApplicationController
   def index
     @start_date = params[:start_date] || (Date.today - 6.days).to_s
     @end_date = params[:end_date] || Date.today.to_s
-    @counter_analytics = current_user
-      .counter_analytics
-      .where("created_at BETWEEN ? AND ?", @start_date + ' 00:00:00', @end_date + ' 23:59:59')
-      .order(created_at: :desc)
-    @length_analytics = current_user
-      .length_analytics
-      .where("created_at BETWEEN ? AND ?", @start_date + ' 00:00:00', @end_date + ' 23:59:59')
-      .order(created_at: :desc)
-    @unique_analytics = current_user
-      .unique_analytics
-      .where("created_at BETWEEN ? AND ?", @start_date + ' 00:00:00', @end_date + ' 23:59:59')
-      .order(created_at: :desc)
-    @browser_analytics = current_user
-      .browser_analytics
-      .where("created_at BETWEEN ? AND ?", @start_date + ' 00:00:00', @end_date + ' 23:59:59')
-      .order(created_at: :desc)
+    %w(counter_analytics length_analytics unique_analytics browser_analytics).each do |analytics|
+      instance_variable_set("@#{analytics}", current_user
+                                      .send(analytics)
+                                      .where("created_at BETWEEN ? AND ?", @start_date + ' 00:00:00', @end_date + ' 23:59:59')
+                                      .order(created_at: :desc))
+    end
   end
 end
