140288393ebe0029bd227976dc3e1be1c069c978
Elson Otake <elsonotake@yahoo.com.br>
2023-12-01 01:20:46 -0300

    Disable background job

--- app/controllers/concerns/track_event.rb
+++ app/controllers/concerns/track_event.rb
@@ -2,14 +2,14 @@ module TrackEvent
   extend ActiveSupport::Concern
 
   def track_event
-    CreateCounterJob.perform_async(session[:action], session[:post_author], current_visitor.id)
-    CreateLengthJob.perform_async(session[:post_author], session[:comment_length]) if session[:action] == 'create'
-    CreateBrowserJob.perform_async(session[:post_author], current_visitor.id, current_visitor.user_agent)
-    CreateUniqueJob.perform_async(session[:post_author], current_visitor.id)
-    # create_counter(session[:action], session[:post_author], current_visitor.id)
-    # create_length(session[:post_author], session[:comment_length]) if session[:action] == 'create'
-    # create_browser(session[:post_author], current_visitor.id, current_visitor.user_agent)
-    # create_unique(session[:post_author], current_visitor.id)
+    # CreateCounterJob.perform_async(session[:action], session[:post_author], current_visitor.id)
+    # CreateLengthJob.perform_async(session[:post_author], session[:comment_length]) if session[:action] == 'create'
+    # CreateBrowserJob.perform_async(session[:post_author], current_visitor.id, current_visitor.user_agent)
+    # CreateUniqueJob.perform_async(session[:post_author], current_visitor.id)
+    create_counter(session[:action], session[:post_author], current_visitor.id)
+    create_length(session[:post_author], session[:comment_length]) if session[:action] == 'create'
+    create_browser(session[:post_author], current_visitor.id, current_visitor.user_agent)
+    create_unique(session[:post_author], current_visitor.id)
   end
 
   def create_counter(action, member, visitor)
