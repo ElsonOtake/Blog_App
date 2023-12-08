81a8abb89d2f4e3e44e44ea1ac290ec55d5eaf04
Elson Otake <elsonotake@yahoo.com.br>
2023-12-08 11:34:14 -0300

    Create add browser active job

--- app/controllers/concerns/track_event.rb
+++ app/controllers/concerns/track_event.rb
@@ -9,6 +9,8 @@ module TrackEvent
 
     AddCounterJob.perform_later(session[:action], session[:post_author], current_visitor.id)
     AddLengthJob.perform_later(session[:post_author], session[:comment_length]) if session[:action] == 'create'
+    AddBrowserJob.perform_later(session[:post_author], current_visitor.id, current_visitor.user_agent)
+    # AddUniqueJob.perform_later(session[:post_author], current_visitor.id)
 
     # create_counter(session[:action], session[:post_author], current_visitor.id)
     # create_length(session[:post_author], session[:comment_length]) if session[:action] == 'create'
