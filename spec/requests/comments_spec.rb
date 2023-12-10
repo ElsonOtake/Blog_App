bf810a5b526d51a1973e1c7c4cf3074e6e4dc2a9
Elson Otake <elsonotake@yahoo.com.br>
2023-12-10 14:37:35 -0300

    Update double to single quotes

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Comments', type: :request do
   before(:each) do
     @member = FactoryBot.create(:member)
-    @post = @member.posts.create(title: "MyTitle", text: "MyText")
+    @post = @member.posts.create(title: 'MyTitle', text: 'MyText')
     sign_in @member
   end
   describe 'Test instance variables' do
@@ -15,15 +15,14 @@ RSpec.describe 'Comments', type: :request do
     end
   end
   describe 'GET /members/:@member/posts/:post/comments/new' do
-    it "render template new" do
+    it 'render template new' do
       get new_member_post_comment_path(@member, @post)
       expect(response).to render_template(:new)
     end
   end
   describe 'POST /members/:@member/posts/:post/comments' do
-    it "return http success" do
-      current_user = @member
-      post member_post_comments_path(@member, @post), params: { comment: {text: "My Text"} }
+    it 'return http success' do
+      post member_post_comments_path(@member, @post), params: { comment: { text: 'My Text' } }
       expect(response).to_not have_http_status(:success)
     end
   end
